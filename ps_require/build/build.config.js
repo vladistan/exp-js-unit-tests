bf90dff8f80cd649c179020523a93ed134ba9a55
Vlad <vlad@v-lad.org>
2017-06-27 22:52:43 -0700

    Build dir

---
+++ ps_require/build/build.config.js
@@ -0,0 +1,9 @@
+({
+	name: "main",
+	baseUrl: "../src/js",
+	mainConfigFile: "../src/js/main.js",
+	out: "../src/js/main-optimized.min.js",
+	generateSourceMaps: true,
+	preserveLicenseComments: false,
+	optimize: "none"
+})
