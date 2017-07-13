d8808c76537766256d786c7a25d15f166a1c2b93
Vlad <vlad@v-lad.org>
2017-07-13 06:34:54 +0000

    With Contact Form Submission

--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MessageBoard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -31,5 +32,12 @@ namespace MessageBoard.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            return View();
+        }
     }
+
 }
