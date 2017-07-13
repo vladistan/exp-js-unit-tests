350f07e3ec0a01a01e57e2f62cdcd3ceb78ec90a
Vlad <vlad@v-lad.org>
2017-07-13 07:41:10 +0000

    With Mail,  Mock, NInject and custom route

--- MessageBoard/App_Start/RouteConfig.cs
+++ MessageBoard/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace MessageBoard
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "MyMessages",
+                url: "MyMessages",
+                defaults: new { controller = "Home", action = "MyMessages"});
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MessageBoard.Services;
 
 namespace MessageBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+             _mail = mail;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -33,9 +41,29 @@ namespace MessageBoard.Controllers
             return View();
         }
 
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+
         [HttpPost]
         public ActionResult Contact(ContactModel model)
         {
+
+            var msg = String.Format("From: {1}{0}Subject: {2}{0}Email:{3}{0}",
+                Environment.NewLine,
+                model.Name,
+                model.Website,
+                model.Comments
+            );
+
+            var rv = _mail.SendMail("vlad@v-lad.org", "amazon@v-lad.org", "Site Comment", msg);
+
+            if (rv)
+            {
+                ViewBag.MailSent = true;
+            }
+
             return View();
         }
     }
