4b435197b5a68ac3f4ad88736878317636a2fa45
Vlad <vlad@v-lad.org>
2017-07-23 03:12:40 +0000

    Data loading from server

--- MessageBoard/js/homeIndex.js
+++ MessageBoard/js/homeIndex.js
@@ -1,10 +1,23 @@
 ﻿var app = angular.module("myApp", []);
 
 
-app.controller("homeIndexController", function ($scope) {
+app.controller("homeIndexController", function ($scope, $http) {
 
     console.log("Inside of the home controller");
 
-    $scope.name = "Bob the operator";
+    $scope.count = 0;
+    $scope.messages = [];
+
+    $http.get("/api/v1/Topics").then(
+        function(result) {
+            console.log("Loaded messages");
+            angular.copy(result.data, $scope.messages);
+        },
+        function() {
+            console.log("Failed to load messages");
+        }
+
+        );
+
 
 });
