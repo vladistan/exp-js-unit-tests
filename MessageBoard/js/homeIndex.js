380b7b4f76264808fe9a1daab77ae12e4ce99627
Vlad <vlad@v-lad.org>
2017-07-23 02:26:57 +0000

    Get angular going

---
+++ MessageBoard/js/homeIndex.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("myApp", []);
+
+
+app.controller("homeIndexController", function ($scope) {
+
+    console.log("Inside of the home controller");
+
+    $scope.name = "Bob the operator";
+
+});
