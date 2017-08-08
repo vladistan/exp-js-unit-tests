ff816cdb7fd1e70bc7d4af6391dbc6f9ac0822b7
Vlad <vlad@v-lad.org>
2017-08-08 05:38:52 +0000

    Add client tests

---
+++ mboard.frontend.test/ClientTests/app.tests.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../scripts/jasmine/jasmine.js"/>
+/// <reference path="../../messageboard/js/app.js" />
+
+
+describe("myapp -> ", function() {
+
+    it("Is Debug",
+        function() {
+            var a = 12;
+            var b = 7;
+            var c = a * b;
+            expect(napp.isDebug).toEqual(true);
+        });
+
+    it(" has B with correct value",
+        function() {
+            expect(b).toEqual(12);
+        });
+
+    it(" has defined app",
+        function() {
+
+            expect(napp).not.toBe(0);
+
+        });
+
+    it("log",
+        function() {
+            expect(napp.log).toBeDefined();
+        });
+
+
+})
---
+++ mboard.frontend.test/ClientTests/homeIndexTest.js
@@ -0,0 +1,114 @@
+﻿/// <reference path="../scripts/jasmine/jasmine.js"/>
+/// <reference path="../../messageboard/scripts/angular.min.js" />
+/// <reference path="../../messageboard/scripts/angular-mocks.js" />
+/// <reference path="../../messageboard/scripts/angular-route.js" />
+/// <reference path="../../messageboard/js/homeindex.js" />
+
+/// <reference path="../../messageboard/js/hojjmeindex.js" />
+
+
+describe("Home Index Tests -> ",
+    function () {
+
+        var $httpBackend;
+
+        beforeEach(function() {
+            module('homeIndex');
+        });
+
+        beforeEach(inject(function ($injector) {
+            $httpBackend = $injector.get("$httpBackend");
+
+            $httpBackend.when("GET", "/api/v1/Topics?includeReplies=True").
+                respond([
+                    {
+                        title: 'Hello',
+                        body: 'Hi there',
+                        id: 5,
+                        created: "20050101"
+                    }]);
+
+        }));
+
+        afterEach(function () {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        describe("App -> ",
+            function() {
+
+                it("Has correct holder variable",
+                    function() {
+                        expect(app).not.toBeNull();
+                    });
+
+            });
+
+        describe("TopicsController -> ", function() {
+
+            it('loads data',
+                inject(function ($controller, $http, dataservice) {
+
+                    var theScope = {};
+
+                    var ctrl = $controller('topicsController',
+                        {
+                            $scope: theScope,
+                            dataservice: dataservice,
+                            $http: $http
+                        });
+
+                    expect(theScope.isBusy).toBe(true);
+
+
+                    $httpBackend.flush();
+
+                    expect(ctrl).not.toBeNull();
+                    expect(theScope.data).toBeDefined();
+                    expect(theScope.data.topics.length).toBe(1);
+                    expect(theScope.data.topics[0].body).toBe('Hi there');
+                    expect(theScope.data.topics[0].id).toBe(5);
+                    expect(theScope.data.topics[0].title).toBe('Hello');
+                    expect(theScope.isBusy).toBe(false);
+
+
+
+                }));
+        });
+
+        describe("DataService -> ",
+            function() {
+
+                it("Has inject",
+                    function() {
+                        expect(inject).not.toBeNull();
+                    });
+
+                it("Can load topics", function() {
+                    inject(function(dataservice) {
+
+                        expect(dataservice.topics).toEqual([]);
+
+                        $httpBackend.expectGET("/api/v1/Topics?includeReplies=True");
+                        dataservice.getTopics().then(
+                            function() {
+                                expect(dataservice.topics.length).toBeGreaterThan(0);
+                                expect(dataservice.topics[0].body).toBe('Hi there');
+                                expect(dataservice.topics[0].title).toBe('Hello');
+                                expect(dataservice.topics[0].id).toBe(5);
+
+
+                            },
+                            function() {
+                                fail();
+
+                            });
+
+                    });
+
+                    $httpBackend.flush();
+                });
+
+            });
+    });
